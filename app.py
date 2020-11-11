1dd065a875bcab118117f95d6a9711951f989e11
Jeyson Calvache <jeison130@gmail.com>
2020-11-10 20:21:38 -0500

    Layouts y pagina de contacto

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 # Rutas
-@app.route('/') # / significa la ruta raiz
+@app.route('/', methods=['GET']) # / significa la ruta raiz
 def index():
     return render_template('index.html')
 
@@ -12,10 +12,22 @@ def saludar(nombre, edad):
     numeros = [1,2,3,4,5,6,7,8,9]
     return render_template('saludo.html', name=nombre, age=edad, numbers=numeros)
 
-@app.route('/contacto')
+@app.route('/contacto', methods=['GET', 'POST'])
 def contacto():
-    return 'En la página de contacto'
+    #Obteniendo formulario de contacto
+    if request.method == 'GET':
+        return render_template('contacto.html')
+    
+    #Guardando la información de contacto
+    nombres = request.form.get('nombres')
+    email = request.form.get('email')
+    celular = request.form.get('celular')
+    observacion = request.form.get('observacion')
 
+    
+
+    return 'Guardando información ' + observacion
+    
 @app.route('/sumar')
 def sumar():
     resultado = 2+2
