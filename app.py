3ba2cd5d2b9f94cb970482fd0e8e56dc8eec7d25
Jeyson Calvache <jeison130@gmail.com>
2020-11-17 21:04:36 -0500

    Estilos y crud usuarios (Listar y crear)

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
+db = sqlite3.connect('data.db', check_same_thread=False)
 
 # Rutas
 @app.route('/', methods=['GET']) # / significa la ruta raiz
@@ -32,5 +34,36 @@ def contacto():
 def sumar():
     resultado = 2+2
     return 'la suma de 2+2=' + str(resultado)
+
+@app.route('/usuarios')
+def usuarios():
+    usuarios = db.execute('select * from usuarios')
+
+    usuarios = usuarios.fetchall()
+
+    return render_template('usuarios/listar.html', usuarios=usuarios)
+
+@app.route('/usuarios/crear', methods=['GET', 'POST'])
+def crear_usuarios():
+    if request.method == 'GET':
+        return render_template('usuarios/crear.html')
+    
+    nombres = request.form.get('nombres')
+    apellidos = request.form.get('apellidos')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    cursor = db.cursor()
+    cursor.execute("""insert into usuarios(
+            nombres,
+            apellidos,
+            email,
+            password
+        )values (?,?,?,?)
+    """, (nombres, apellidos, email, password))
+
+    db.commit()
+
+    return redirect(url_for('usuarios'))
 
 app.run(debug=True)
