c9fed27e33380af3e4a70ca37b4c6b81d531a14a
Jeyson Calvache <jeison130@gmail.com>
2020-12-01 19:48:29 -0500

    Cambios

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = 'spbYO0JJOPUFLUikKYbKrpS5w3KUEnab5KcYDdYb'
 db = sqlite3.connect('data.db', check_same_thread=False)
 
 # Rutas
@@ -25,8 +26,6 @@ def contacto():
     email = request.form.get('email')
     celular = request.form.get('celular')
     observacion = request.form.get('observacion')
-
-    
 
     return 'Guardando información ' + observacion
     
@@ -53,16 +52,56 @@ def crear_usuarios():
     email = request.form.get('email')
     password = request.form.get('password')
 
+    #Validando que el correo no este en uso
+    usuario = db.execute('select * from usuarios where email = ?', (email,)).fetchall()
+
+    if(nombres == ""):
+        flash('El campo nombres es requerido', 'error')
+        return redirect(request.url)
+
+    if(len(usuario) > 0):
+        flash('Ya existe un usuario con este email', 'error')
+        return redirect(request.url)
+
+    try:
+        cursor = db.cursor()
+        cursor.execute("""insert into usuarios(
+                nombres,
+                apellidos,
+                email,
+                password
+            )values (?,?,?,?)
+        """, (nombres, apellidos, email, password,))
+
+        db.commit()
+    except:
+        flash('No se ha podido guardar el usuario', 'error')
+        return redirect(url_for('usuarios'))
+
+    flash('Usuario creado correctamente', 'success')
+
+    return redirect(url_for('usuarios'))
+
+@app.route('/usuarios/editar/<int:id>', methods=['GET', 'POST'])
+def editar_usuario(id):
+    if request.method == 'GET':
+        usuario = db.execute('select * from usuarios where id = ?', (id,)).fetchone()
+        
+        return render_template('usuarios/editar.html', usuario=usuario)
+
+    nombres = request.form.get('nombres')
+    apellidos = request.form.get('apellidos')
+    email = request.form.get('email')
+    # password = request.form.get('password')
+
     cursor = db.cursor()
-    cursor.execute("""insert into usuarios(
-            nombres,
-            apellidos,
-            email,
-            password
-        )values (?,?,?,?)
-    """, (nombres, apellidos, email, password))
+    cursor.execute("""update usuarios set nombres = ?,
+        apellidos = ?, email = ? where id = ? 
+    """, (nombres, apellidos, email, id,))
 
     db.commit()
+
+    flash('Usuario editado correctamente', 'success')
 
     return redirect(url_for('usuarios'))
 
